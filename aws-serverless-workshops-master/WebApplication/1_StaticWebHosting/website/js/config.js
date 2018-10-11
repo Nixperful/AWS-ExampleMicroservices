b6fd6f2f725906615109fe980148add2ca12aefa
Nixperful <nicolasosorioarias@hotmail.com>
2018-10-10 22:04:14 -0500

    Taller AREM Segundo Corte AWS Microservicios

---
+++ aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_ZO9SBuPtt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'uht2qp4ga0hch9eut76afjde5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://a20hwfp8v0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
